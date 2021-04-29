515425aa55c3a73d3274d210c09449b1c08b9c0d
Brayan Miranda <brayanmiranda333@gmail.com>
2021-04-29 13:00:17 -0600

    v1.1 test, pruebas de integracion estudiante completas

--- src/test/java/com/demo/student/IntegrationTest/GetMethods.java
+++ src/test/java/com/demo/student/IntegrationTest/GetMethods.java
@@ -1,6 +1,43 @@
 package com.demo.student.IntegrationTest;
 
+import com.demo.student.Student.Student;
+import com.demo.student.Student.StudentController;
+import com.demo.student.Student.StudentRepository;
+import com.demo.student.Student.StudentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootConfiguration(class)
+@AutoConfigureMockMvc
+@WebMvcTest(StudentController.class)
 public class GetMethods {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private StudentService underTestService;
+
+    @Autowired
+    private StudentRepository underTestRepository;
+
+
+
+    @Test
+    public void getStudent (){
+        List<Student> studentList = underTestService.getStudent();
+        System.out.println(studentList);
+    }
 
 
 }
--- src/test/java/com/demo/student/StudentApplicationTests.java
+++ src/test/java/com/demo/student/StudentApplicationTests.java
@@ -1,13 +1,22 @@
 package com.demo.student;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.demo.student.Student.StudentController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentApplicationTests {
 
+	@Autowired
+	private StudentController stdControler;
+
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(stdControler).isNotNull();
+
 	}
 
 }
